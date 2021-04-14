3c83ea3b2a980837599aa8a10f6d99243fc4f9dd
yasinaktimur <yasinaktimur@gmail.com>
2021-04-14 15:02:20 +0300

    formatic

--- docs/wc.js
+++ docs/wc.js
@@ -38,7 +38,14 @@ function init() {
       package: WalletConnectProvider,
       rpc: {
         1: "https://bsc-dataseed.binance.org/",
-      }
+      },
+
+      fortmatic: {
+     package: Fortmatic,
+     options: {
+       // Mikko's TESTNET api key
+       key: "pk_test_391E26A3B43A3350"
+     }
     }
   };
 
