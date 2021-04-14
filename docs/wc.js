b173cf187051e4bee07b56e38d9f47ed84917729
yasinaktimur <yasinaktimur@gmail.com>
2021-04-14 14:04:02 +0300

    eth_sendRawTransaction

--- docs/wc.js
+++ docs/wc.js
@@ -213,7 +213,7 @@ async function generateMemo() {
                  'nonce':  web3.utils.toHex(nonce) }
                  console.log(rawTx);
 
-                 const txhash  = provider.sendAsync(rawTx).then(function(xxx) {
+                 provider.eth_sendRawTransaction(rawTx).then(function(xxx) {
                    console.log("my nonce value is here:", xxx);
                  }).catch(function(err){ console.log("gasolina err", err); });
 
